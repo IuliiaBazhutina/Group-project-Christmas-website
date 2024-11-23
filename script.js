96a5607d40b56e6d731fe6ee3c5a299f8efe0302
Raj Patel <raj.patel514@gmail.com>
2024-11-23 10:12:01 -0500

    Created Array for fun fact of the day

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// JS for Home Page
+
+//https://thefactfile.org/interesting-facts-christmas/
+let messages = [
+    "The image of Santa Claus flying in his sleigh first appeared in 1819",
+    "The original Rudolph did not have a red nose",
+    "America’s first batch of eggnog was made in the Jamestown settlement in 1607",
+    "Some zoos take donated Christmas trees and use them as food for the animals",
+    "In 1918, the city of Boston received a giant Christmas tree as a gift from the Canadian province of Nova Scotia",
+    "Tinsel was invented in 1610 in Germany and was once made of real silver."
+];
+
