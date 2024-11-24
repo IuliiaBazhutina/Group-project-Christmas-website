1a6ac162563c47a6919a084529a68d4c4d27dc5d
Raj Patel <raj.patel514@gmail.com>
2024-11-24 00:44:58 -0500

    Added function to calc distance to North Pole

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // JS for Home Page
 
 //https://thefactfile.org/interesting-facts-christmas/
-
 //ARRAY OF MESSAGES FOR HOMEPAGE
 let messages = [
     "The image of Santa Claus flying in his sleigh first appeared in 1819",
@@ -13,6 +12,7 @@ let messages = [
     "Tinsel was invented in 1610 in Germany and was once made of real silver."
 ];
 
+//////////ADD FUNCTION HERE TO CYCLE THROUGH MESSAGES ON HOME PAGE
 
 //GET LOCATION FOR HOMEPAGE
 const locationButton = document.getElementById("button2");
@@ -21,7 +21,7 @@ locationButton.addEventListener("click", getLocation);
 function getLocation() {
     const locationElement = document.getElementById("location");
     if (navigator.geolocation) {
-        locationElement.innerHTML = "Getting location...";
+        locationElement.innerHTML = "Getting distance...";
 
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -29,11 +29,26 @@ function getLocation() {
     }
 }
 
-function showPosition(position) {
-    const locationElement = document.getElementById("location");
-    locationElement.innerHTML = `Latitude: ${position.coords.latitude}<br>Longitude: ${position.coords.longitude}`;
-}
+//NO LONGER NEEDED AS PER FUNCTION BELOW
+// function showPosition(position) {
+//     const locationElement = document.getElementById("location");
+//     locationElement.innerHTML = `Latitude: ${position.coords.latitude}<br>Longitude: ${position.coords.longitude}`;
+// }
 
 //LOCATION OF THE NORTH POLE
 const northpoleLat = 90;
-const northpoleLong = 0.0002;
+//LONGTITUDE NOT NEEDED FOR NORTHPOLE
+// const northpoleLong = 0.0002;
+
+
+//CALCULATION TO DETERMINE DISTANCE OF USER TO NORTH POLE
+const locationElement = document.getElementById("location");
+const kmPerDegree = 111.32; //Approx value
+
+function showPosition(position) {
+    const userLat = position.coords.latitude;
+    const latitudeDiff = northpoleLat - userLat;
+    const distancetoNP = latitudeDiff * kmPerDegree;
+
+    locationElement.innerHTML = `Your distance from the North Pole is: ${distancetoNP.toFixed(3)} kilometers!`;
+}
