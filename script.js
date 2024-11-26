c2843718115abf0d23f003cf7a71d6db278d5168
Raj Patel <raj.patel514@gmail.com>
2024-11-25 20:06:37 -0500

    Added xmas countdown clock to JS and HTML

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 // JS for Home Page
 
+//COUNTDOWN TIMER
+//https://www.w3schools.com/howto/howto_js_countdown.asp
+var countDownDate = new Date("December 25, 2024 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + " Days " + hours + " Hours "
+        + minutes + " Minutes " + seconds + " Seconds, Until CHRISTMAS! ";
+}, 1000);
+
 //https://thefactfile.org/interesting-facts-christmas/
 //ARRAY OF MESSAGES FOR HOMEPAGE
 let messages = [
