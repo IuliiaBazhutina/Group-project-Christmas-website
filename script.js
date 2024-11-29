4317330d6b8b2cdc7f65849796e7b91e82feac15
Raj Patel <raj.patel514@gmail.com>
2024-11-29 13:01:17 -0500

    Modified CSS and HTML functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //COUNTDOWN FUNCTION TO DISPLAY THE TIME UNTIL XMAS
 var countDownDate = new Date("December 25, 2024 00:00:00").getTime(),
     x = setInterval(function () {
@@ -16,27 +15,14 @@ var countDownDate = new Date("December 25, 2024 00:00:00").getTime(),
     }, 1e3);
 
 //ARRAY OF MESSAGES FOR THE FACT OF THE DAY
-
-// JS for Home Page
-
-//COUNTDOWN TIMER
-//https://www.w3schools.com/howto/howto_js_countdown.asp
-var countDownDate = new Date("December 25, 2024 00:00:00").getTime();
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown").innerHTML = days + " Days " + hours + " Hours "
-        + minutes + " Minutes " + seconds + " Seconds ";
-}, 1000);
-
-//https://thefactfile.org/interesting-facts-christmas/
-//ARRAY OF MESSAGES FOR HOMEPAGE
-
+let messages = [
+    "The image of Santa Claus flying in his sleigh first appeared in 1819",
+    "The original Rudolph did not have a red nose",
+    "America’s first batch of eggnog was made in the Jamestown settlement in 1607",
+    "Some zoos take donated Christmas trees and use them as food for the animals",
+    "In 1918, the city of Boston received a giant Christmas tree as a gift from the Canadian province of Nova Scotia",
+    "Tinsel was invented in 1610 in Germany and was once made of real silver.",
+    "Christmas might be a public holiday; however, it is not a biblical holy day. The Bible does not say anywhere to keep Christmas as a holy day.",
     "The ‘X’ in X-Mas, as we all use today, comes from the Greek meaning of ‘X’ i.e. Christ.",
 ],
     msgIndex = 0;
